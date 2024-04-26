b434da82fdc33b832bd0f2e007b99299aeb02252
dracx <dracx.py@gmail.com>
2024-04-26 11:37:51 +0530

    .

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
 from api.main import app
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, log_level="debug", debug=True,
-                workers=1, limit_concurrency=1, limit_max_requests=1)
