b0406863f5485e63da35577b6ac97d5d606aac18
dracx <dracx.py@gmail.com>
2024-04-29 19:20:42 +0530

    .

--- api/db/database.py
+++ api/db/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-engine = create_engine("sqlite:///./mydb.db", connect_args={"check_same_thread": False})
+#engine = create_engine("sqlite:///./mydb.db", connect_args={"check_same_thread": False})
+engine = create_engine(os.getenv("DATABASE_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
